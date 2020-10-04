2d2a44fa5ac5104cb34a50860570d4a78168e350
Alexandros Naskos <alex_naskos@hotmail.com>
2020-10-04 19:24:21 +0300

    Start creating a Compilation isntance

--- build.zig
+++ build.zig
@@ -1,14 +1,34 @@
-const Builder = @import("std").build.Builder;
+const std = @import("std");
+const Builder = std.build.Builder;
+const Pkg = std.build.Pkg;
 
 pub fn build(b: *Builder) void {
     const mode = b.standardReleaseOptions();
     const lib = b.addStaticLibrary("zig-wasi", "src/main.zig");
+
     lib.setBuildMode(mode);
+    lib.addBuildOption(bool, "is_stage1", false);
+    lib.addBuildOption(bool, "have_llvm", false);
+    lib.addBuildOption(bool, "enable_tracy", false);
+    lib.addBuildOption([]const u8, "version", "0.6.0-modded");
+
+    // @TODO Better way to inherit te build options package.
+    const build_options_file = std.fs.path.join(
+        b.allocator,
+        &[_][]const u8{ b.cache_root, b.fmt("{}_build_options.zig", .{lib.name}) },
+    ) catch unreachable;
+
+    lib.addPackage(.{
+        .name = "zig",
+        .path = "zig/src/main.zig",
+        .dependencies = &[_]Pkg{.{
+            .name = "build_options",
+            .path = b.pathFromRoot(build_options_file),
+        }},
+    });
+    lib.setTarget(.{
+        .cpu_arch = .wasm32,
+        .os_tag = .wasi,
+    });
     lib.install();
-
-    var main_tests = b.addTest("src/main.zig");
-    main_tests.setBuildMode(mode);
-
-    const test_step = b.step("test", "Run library tests");
-    test_step.dependOn(&main_tests.step);
 }
--- src/main.zig
+++ src/main.zig
@@ -1,10 +1,66 @@
 const std = @import("std");
-const testing = std.testing;
+const zig = @import("zig");
 
-export fn add(a: i32, b: i32) i32 {
-    return a + b;
+comptime {
+    std.debug.assert(std.builtin.arch == .wasm32);
+    std.debug.assert(std.builtin.os.tag == .wasi);
 }
 
-test "basic add functionality" {
-    testing.expect(add(3, 7) == 10);
+export fn buildIR(gpa: *std.mem.Allocator, directory: []const u8, file_name: []const u8) callconv(.Unspecified) !void {
+    var preopens = std.fs.wasi.PreopenList.init(std.testing.allocator);
+    defer preopens.deinit();
+    try preopens.populate();
+
+    const PreopenType = std.fs.wasi.PreopenType;
+    const lib_preopen = preopens.find(PreopenType{ .Dir = "lib" }) orelse return error.LibDirectoryNotInPreopens;
+    const cache_preopen = preopens.find(PreopenType{ .Dir = "zig-cache" }) orelse return error.CacheDirectoryNotInPreopens;
+    const global_cache_preopen = preopens.find(PreopenType{ .Dir = ".cache" }) orelse return error.GlobalCacheDirectoryNotInPreopens;
+    const source_directory_preopen = preopens.find(PreopenType{ .Dir = directory }) orelse return error.SourceDirectoryNotInPreopens;
+
+    const preopen = preopens.find(PreopenType{ .Dir = directory }) orelse return error.DirectoryNotInPreopens;
+
+    var root_pkg = zig.Package{
+        .root_src_directory = .{
+            .path = null,
+            .handle = .{ .fd = source_directory_preopen.fd },
+        },
+        .root_src_path = file_name,
+    };
+
+    const random_seed = blk: {
+        var random_seed: u64 = undefined;
+        try std.crypto.randomBytes(std.mem.asBytes(&random_seed));
+        break :blk random_seed;
+    };
+    var default_prng = std.rand.DefaultPrng.init(random_seed);
+
+    const comp = try zig.Compilation.create(gpa, .{
+        .zig_lib_directory = .{
+            .path = null,
+            .handle = .{ .fd = lib_preopen.fd },
+        },
+        .local_cache_directory = .{
+            .path = null,
+            .handle = .{ .fd = cache_preopen.fd },
+        },
+        .global_cache_directory = .{
+            .path = null,
+            .handle = .{ .fd = global_cache_preopen.fd },
+        },
+        .root_name = "root",
+        .target = std.Target{
+            .cpu = std.Target.Cpu.Model.generic(.x86_64).toCpu(.x86_64),
+            .os = std.Target.Os.Tag.linux.defaultVersionRange(),
+            .abi = .none,
+        },
+        .root_pkg = &root_pkg,
+        .output_mode = .Obj,
+        .rand = &default_prng.random,
+        .emit_bin = null, // <- @TODO
+        .object_format = null, // <- @TODO
+        .optimize_mode = .Debug, // <- @TODO
+        .keep_source_files_loaded = true, // true to get ZIR
+        .is_native_os = false,
+        .color = .Off,
+    });
 }
